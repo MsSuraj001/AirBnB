6add21e253f9de483acbfc3fab245b94bd18b0aa
MsSuraj001 <milukumar225@gmail.com>
2024-12-18 17:21:08 +0530

    added the code of connection to the mongodb database

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const ejs = require("ejs");
-const mangoose = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
+
+const MONGODB_URL = "mongodb://127.0.0.1:27017/test";
+
+main().then( () =>{
+    console.log("Connected to MongoDB");
+}).catch( (err)=>{
+    console.log("Error connecting to MongoDB", err);
+})
+
+async function main(){
+    mongoose.connect(MONGODB_URL)
+}
 
 // this is the root dirctery
 app.get("/", (req, res)=>{
